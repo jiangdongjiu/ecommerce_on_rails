fccb76258732d805676edb850d65994501d205f7
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-15 10:16:51 -0600

    order_detail validations

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  validates :order_price, :quantity, presence: true
+  validates :order_price, numericality: true
+  validates :quantity, numericality: { only_integer: true }
 end
