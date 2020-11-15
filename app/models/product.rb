0a3d1a18e92e9c9b2e31fde3e4fd835aa1e11e1e
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-15 09:59:15 -0600

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_details
+
+  validates :name, :price, :stock, :description, presence: true
+  validates :price, numericality: true
+  validates :stock, numericality: { only_integer: true }
 end
