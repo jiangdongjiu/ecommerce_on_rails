7126bb74f34437f4a5c417d634d8968d64162794
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-15 18:36:34 -0600

    add product controller placeholder. edit customers whitelisting

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :username, :password, :fullname, :email, :billing_address, :default_shipping_address, :phone, :province_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:username, :password, :fullname, :email, :billing_address, :default_shipping_address, :phone, :province_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  # whitelisting
+  permit_params :username,
+                :password,
+                :phone,
+                :email,
+                :fullname,
+                :billing_address,
+                :default_shipping_address,
+                :province_id
 end
