f0b5fd5154637e1822514099004b0eab9245426a
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-19 13:23:23 -0600

    multiple upload image in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
+require 'open-uri'
+
 OrderDetail.delete_all
 Product.delete_all
-Category.delete_all
+# Category.delete_all
 Order.delete_all
 Customer.delete_all
 Province.delete_all
@@ -8,14 +10,14 @@ AdminUser.delete_all
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-categories = ["Meat and Veggies", "Easy and Fast", "Family Friendly", "Vegetarian"]
+# categories = ["Meat and Veggies", "Easy and Fast", "Family Friendly", "Vegetarian"]
 def random_category()
   ["Meat and Veggies", "Easy and Fast", "Family Friendly", "Vegetarian"].sample
 end
 
-categories.each do |category|
-  Category.create(name: category)
-end
+# categories.each do |category|
+#   Category.create(name: category)
+# end
 
 def api_fetch(url)
   JSON.parse(URI.open(url).read)
@@ -32,12 +34,21 @@ page_numbers.each do |page|
   recipes += recipe_page["results"]
 end
 
-# p recipes[0]
 recipes.each do |recipe|
-  Product.create(name: recipe["title"],
-                price: Faker::Commerce.price,
-                description: "Ingredients: " + recipe["ingredients"],
-                stock: 100,
-                category: Category.find_by(name: random_category()),
-                image_url: recipe["thumbnail"])
+  recipe_entry = Product.create(name: recipe["title"],
+                          price: Faker::Commerce.price,
+                          description: "Ingredients: " + recipe["ingredients"],
+                          stock: 100,
+                          category: Category.find_by(name: random_category()),
+                          image_url: recipe["thumbnail"])
+  if recipe["thumbnail"].present?
+    downloaded_image = URI.open("#{recipe["thumbnail"]}")
+  else
+    downloaded_image = URI.open("http://img.recipepuppy.com/9.jpg")
+  end
+
+  if downloaded_image
+    recipe_entry.image.attach(io: downloaded_image, filename: "#{recipe["title"]}.jpg")
+  end
+  # sleep(1)
 end
