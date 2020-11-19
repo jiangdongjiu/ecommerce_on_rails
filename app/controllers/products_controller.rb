3528cfb42fa7c7ce8e623b99a1d3d59770858cf8
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-18 21:44:52 -0600

    add different category view using partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def vegetarian
+    @products = Product.where(category_id: 13)
+  end
+
+  def family_friendly
+    @products = Product.where(category_id: 12)
+  end
+
+  def easy_fast
+    @products = Product.where(category_id: 11)
+  end
+
+  def meat_veggies
+    @products = Product.where(category_id: 10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   root to: 'products#index'
-  resources :products, only: [:index, :show]
+
+  resources :products, only: [:index, :show] do
+    collection do
+      get 'vegetarian'
+    end
+    collection do
+      get 'family_friendly'
+    end
+    collection do
+      get 'easy_fast'
+    end
+    collection do
+      get 'meat_veggies'
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
