f644dc3d2856df8bdfb1a02638ace91c3222edcb
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-19 06:07:41 -0600

    install acrive storage: rails active_storage:install

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,6 @@ class Product < ApplicationRecord
   validates :name, :price, :stock, :description, presence: true
   validates :price, numericality: true
   validates :stock, numericality: { only_integer: true }
+
+  has_one_attached :image
 end
