56fc88bf2ae272157f5acf7809358805faef45f6
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-19 18:12:58 -0600

    change view show description only in show view and show categor of each recipe

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,25 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.includes(:category).find(params[:id])
   end
 
   def vegetarian
-    @products = Product.where(category_id: 13)
+    @products = Product.includes(:category).where(category_id: 13)
   end
 
   def family_friendly
-    @products = Product.where(category_id: 12)
+    @products = Product.includes(:category).where(category_id: 12)
   end
 
   def easy_fast
-    @products = Product.where(category_id: 11)
+    @products = Product.includes(:category).where(category_id: 11)
   end
 
   def meat_veggies
-    @products = Product.where(category_id: 10)
+    @products = Product.includes(:category).where(category_id: 10)
   end
 end
