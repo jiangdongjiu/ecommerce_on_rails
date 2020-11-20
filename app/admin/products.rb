6a556fed7984c0ec568ad3ff13f49e9e1964e7fa
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-20 02:46:25 -0600

    show image in active admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -28,7 +28,7 @@ ActiveAdmin.register Product do
       f.input :description
       f.input :stock
       f.input :image_url
-      f.input :image, as: :file
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
       f.input :category, as: :select
       f.has_many :order_details, allow_destroy: true do |n_f|
         n_f.input :order
