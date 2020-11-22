c7467e004667e0e51afbf5cd5071522eaafd9832
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-21 22:12:06 -0600

    add and remove recipe in shopping cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,11 +3,23 @@ class CartController < ApplicationController
   # Data sent as form data (params)
   def create
     # Add param[:id] to cart.
-    logger.debug("Adding #{params[:id]} to the cart.")
+    id = params[:id].to_i
+
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      flash[:notice] = "#{Product.find(id).name} added to your cart."
+    end
+
+    redirect_to root_path
+    # logger.debug("Adding #{params[:id]} to the cart.") #termial output
   end
 
   # DELETE /cart/:id
   def destroy
     # Remove param[:id] from cart
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    flash[:notice] = "#{Product.find(id).name} removed from your cart."
+    redirect_to root_path
   end
 end
