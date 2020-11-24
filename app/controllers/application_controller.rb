58704c8a2ea3e9cb7b64b6622058d9f75db91558
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-23 20:12:09 -0600

    edit application_controller to add fields in signup form of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :initialize_session
   helper_method :cart
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:default_shipping_address, :province_id])
+  end
+
   private
 
   def initialize_session
