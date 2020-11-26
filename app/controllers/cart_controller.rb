335fe84d25883b50cd4bff7d2c0ae366ced9f9ce
jiangdongjiu <jiangdongjiu5@gmail.com>
2020-11-26 13:45:33 -0600

    bulma style notice

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,7 +6,7 @@ class CartController < ApplicationController
     id = params[:id].to_i
     quantity = params[:recipe]["quantity"].to_i
     session[:shopping_cart][id] = quantity # hash: {1 => 5, 2 => 6} product(id = 1) have 5 quantity
-    flash[:notice] = "#{Product.find(id).name} added to your cart."
+    flash[:note] = "#{Product.find(id).name} added to your cart."
 
     redirect_to root_path
     # logger.debug("Adding #{params[:id]} to the cart.") #termial output
@@ -17,12 +17,14 @@ class CartController < ApplicationController
     # Remove param[:id] from cart
     id = params[:id].to_i
     session[:shopping_cart].delete(id.to_s)
-    flash[:notice] = "#{Product.find(id).name} removed from your cart."
+    flash[:note] = "#{Product.find(id).name} removed from your cart."
     redirect_to root_path
   end
 
   def checkout
-    @cart = cart
-    @province = Province.all
+    unless user_signed_in?
+      flash[:signin] = "Please sign in."
+      redirect_to root_path
+    end
   end
 end
